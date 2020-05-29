9d13159ee508d99a5ac139db7b74ea19c2dbc760
ahiezerrivas <ahiezerrivas@gmail.com>
2020-05-29 14:01:03 -0400

    video 77 technology card

--- section 09/BACKENDVSFRONTEND/frontend/src/app/components/technology-card/technology-card.component.ts
+++ section 09/BACKENDVSFRONTEND/frontend/src/app/components/technology-card/technology-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Technology } from 'src/app/models/technology.model";
 
 @Component({
   selector: 'app-technology-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technology-card.component.css']
 })
 export class TechnologyCardComponent implements OnInit {
-
+  @Input () technology: any = {};
   constructor() { }
 
   ngOnInit(): void {
