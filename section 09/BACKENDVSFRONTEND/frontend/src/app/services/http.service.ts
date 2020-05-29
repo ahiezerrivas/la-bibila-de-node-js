d1613e858c6cb009bdef5c75682a9937586f30eb
ahiezerrivas <ahiezerrivas@gmail.com>
2020-05-29 11:55:36 -0400

    video 76 txt final

--- section 09/BACKENDVSFRONTEND/frontend/src/app/services/http.service.ts
+++ section 09/BACKENDVSFRONTEND/frontend/src/app/services/http.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Technology } from '../models/technology.model';
+import { environment } from 'src/environments/enviroment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
   constructor(private readonly _http: HttpClient) { }
+  
+  public getTechnologies() {
+    return this._http.get<Technology[]>(this.baseUrl + "/technologies");
+  }
+  
+  public getTechnology(id: string) {
+    return this._http.get<Technology>(this.baseUrl + "/technology/" + id);
+  }
+  
+  public searchTechnology(query: string) {
+    return this._http.get<Technology>(
+      this.baseUrl + "/technology/search/" + query
+      );
+  }
+  
 }
